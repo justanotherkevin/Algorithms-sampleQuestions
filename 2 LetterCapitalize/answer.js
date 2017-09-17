63be17495a88f9d03714ff167443635ce15dc9b1
kevin hu <hu.kevin.w@gmail.com>
2017-09-17 16:59:58 -0400

    Add answer to letter capitalize question

---
+++ 2 LetterCapitalize/answer.js
@@ -0,0 +1,27 @@
+// Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space.
+//
+function LetterCapitalize(str) {
+
+  // code goes here
+  let hello = str.split(" ")
+  let new_hello = []
+  for (const one of hello) {
+      new_hello.push( one.replace( one[0], one[0].toUpperCase() ) )
+  }
+
+  return new_hello.join(' ');
+
+}
+
+
+// their answer
+function LetterCapitalize(str) {
+
+  // our regex [a-z] matches every alphabetic character in the string
+  // but the \b before it specifies a word boundary, in other words, nothing can 
+  // come before those letters therefore selecting every word in the string
+  return str.replace(/\b[a-z]/gi, function(char) {
+    return char.toUpperCase();
+  });
+
+}
