533be814b8c3fc0efa9a9ff10ebb1f483c82ba6e
Kevin Hu <hu.kevin.w@gmail.com>
2019-01-20 16:57:06 -0500

    Quick sort:

--- sorting/quicksort.js
+++ sorting/quicksort.js
@@ -1,52 +1,73 @@
+/* 
+1. Find a “pivot” item in the array. This item is the basis for comparison for a single round.
+2. Start a pointer (the left pointer) at the first item in the array.
+3. Start a pointer (the right pointer) at the last item in the array.
+4. While the value at the left pointer in the array is less than the pivot value, move the left pointer to the right (add 1). Continue until the value at the left pointer is greater than or equal to the pivot value.
+5. While the value at the right pointer in the array is greater than the pivot value, move the right pointer to the left (subtract 1). Continue until the value at the right pointer is less than or equal to the pivot value.
+6. If the left pointer is less than or equal to the right pointer, then swap the values at these locations in the array.
+7. Move the left pointer to the right by one and the right pointer to the left by one.
+8. If the left pointer and right pointer don’t meet, go to step 1.
+
+https://humanwhocodes.com/blog/2012/11/27/computer-science-in-javascript-quicksort/
+https://www.kirupa.com/sorts/quicksort.htm
+*/
+
 let example = [5,4,2,1,4,5,7,8]
 
-const sort = ( arr ) => {
-  while ( isSorted( arr ) === false ) {
-    quickSort( arr )
-  }
-  return arr 
-}
-const isSorted = (arr) => {
-  for ( let i =0; i< arr.length; i++ ) {
-    if ( arr[i] > arr[i+1]) {
-      return false 
-    }
-  }
-  return true 
-}
-const quickSort = (arr) =>  {
-  let pivotIndex  = Math.floor ( arr.length /2 );
-  let endIndex = arr.length - 1
-  let pivotValue = arr[pivotIndex];
-  let arrCopy = [...arr]  
-  // [5,4,2,1,4,5,7,8] = > [5,4,2,8,4,5,7,1]
-  arrCopy[pivotIndex ] = arrCopy[endIndex]
-  arrCopy[endIndex] = pivotValue
 
-  let fromLeftLargerThanPivot = (arr) => { 
-    for ( let i = 0; i< arr.length; i++ ) {
-      if ( arr[i] > pivotValue) {
-        return i 
-      }
+let swap = (arr, i, j) => {
+  let tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+};
+function partition(items, left, right) {
+  var pivot   = items[Math.floor((right + left) / 2)],
+      i       = left,
+      j       = right;
+  while (i <= j) {
+    while (items[i] < pivot) {
+      i++;
     }
-  }
-  let fromRightSmallerThanPivot = (arr) => {
-    for ( let i = arr.length-2 ;  i >= 0 ; i-- ) {
-      if ( arr[i] < pivotValue) {
-        return i 
-      }
-    }
-  }
-  // [5,4,2,8,4,5,7,1] => 
-  let largerThanPivotIndex = fromLeftLargerThanPivot(arrCopy)
-  let largerVal = arrCopy[largerThanPivotIndex]
-  let smallerThanPivotIndex = fromRightSmallerThanPivot(arrCopy)
-  console.log(largerThanPivotIndex, smallerThanPivotIndex)
-  
-  arrCopy[largerThanPivotIndex] = arrCopy[smallerThanPivotIndex]
-  arrCopy[smallerThanPivotIndex] = largerVal
 
-  return arrCopy
+    while (items[j] > pivot) {
+      j--;
+    }
+
+    if (i <= j) {
+      swap(items, i, j);
+      i++;
+      j--;
+    }
+  }
+  return i;
 }
-// let example = [5,4,2,1,4,5,7,8]
-console.log( quickSort(example)) 
+
+function quickSort(items, left, right) {
+
+  var pivotIndex;
+  left = left || 0; 
+  right = right || items.length -1; 
+  if (items.length > 1) {
+
+      pivotIndex = partition(items, left, right);
+
+      if (left < pivotIndex - 1) {
+          quickSort(items, left, pivotIndex - 1);
+      }
+
+      if (pivotIndex < right) {
+          quickSort(items, pivotIndex, right);
+      }
+
+  }
+
+  return items;
+}
+
+console.log( quickSort(example) );
+
+// [5,4,2,1,4,5,7,8] , 0 , 7 
+
+// 0 to 7  
+
+
